be9bc50e506172147c76cdf3bac5ca90d88cd847
Srikanth <srikanthevergreen@gmail.com>
2021-11-21 16:04:05 +0530

    fixed one task

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String[]) args) {
-		// Prints "Hello, World" in the terminal window.
-		System.out.println("Hello, World");
+		// Prints "Hello, World.. This is for Jenkins practise" in the terminal window.
+		System.out.println("Hello, World.. This is for Jenkins practise");
 	}
 }
